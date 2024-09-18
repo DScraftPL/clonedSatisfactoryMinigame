a5e1906482b031afa2c0d8089ad6adf2b6e8b0bc
DScraftPL <xxxwiacekxxx@gmail.com>
2024-09-18 14:20:59 +0200

    humble beginings, something actually works, refactored to 2d array

--- src/DragAndDropComponent.js
+++ src/DragAndDropComponent.js
@@ -5,16 +5,43 @@ const ItemTypes = {
   BOX: "box",
 };
 
-const DraggableItem = ({ id, color }) => {
+const BlockTypes = {
+  Single: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  },
+  T: {
+    left: 1,
+    right: 1,
+    top: 0,
+    bottom: 1
+  },
+  L: {
+    left: 0,
+    right: 1,
+    top: 2,
+    bottom: 0
+  },
+  I: {
+    left: 0,
+    right: 0,
+    top: 1,
+    bottom: 2
+  }
+}
+
+const DraggableItem = ({ id, color, blockType }) => {
   const [{ isDragging }, drag] = useDrag(
     () => ({
       type: ItemTypes.BOX,
-      item: { id, color },
+      item: { id, color, blockType },
       collect: (monitor) => ({
         isDragging: monitor.isDragging(),
       }),
     }),
-    [id, color]
+    [id, color, blockType]
   );
 
   return (
@@ -26,37 +53,32 @@ const DraggableItem = ({ id, color }) => {
       }}
       className="m-2 cursor-grab w-12 h-12"
     >
-      {id}
+      {blockType}
     </div>
   );
 };
 
-/** 
- * @param {bool} isOver
- * @param {string} color
- * @returns {string}
- */
 const cellGridColor = (isOver, color) => {
-  if (isOver) return "bg-yellow-300";
+  if (isOver) return "bg-white";
 
   switch (color) {
     case "blue":
-      return "bg-blue-300";
+      return "bg-blue-500";
     case "red":
-      return "bg-red-300";
+      return "bg-red-500";
     case "yellow":
-      return "yellow";
+      return "bg-yellow-500";
     case "green":
-      return "bg-green-300";
+      return "bg-green-500";
     default:
-      return "bg-gray-300";
+      return "bg-gray-500";
   }
 };
 
-const CellGrid = ({ onDrop, index, color }) => {
+const CellGrid = ({ onDrop, row, col, color }) => {
   const [{ isOver }, drop] = useDrop(() => ({
     accept: ItemTypes.BOX,
-    drop: (item) => onDrop(index, item.color, item.id),
+    drop: (item) => onDrop(row, col, item.color, item.id, item.blockType),
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
@@ -65,7 +87,7 @@ const CellGrid = ({ onDrop, index, color }) => {
   return (
     <div
       ref={drop}
-      className={`border border-black w-full h-full transition-colors duration-200 ${cellGridColor(
+      className={`aspect-square border border-black h-full w-full transition-colors duration-200 ${cellGridColor(
         isOver,
         color
       )}`}
@@ -75,18 +97,23 @@ const CellGrid = ({ onDrop, index, color }) => {
 
 const DropGrid = ({ handleDrop, cellColor }) => {
   return (
-    <div className="grid grid-cols-5 grid-rows-5 gap-1 w-64 h-64">
-      {Array.from({ length: 25 }, (_, index) => (
-        <CellGrid
-          color={cellColor[index]}
-          key={index}
-          onDrop={handleDrop}
-          index={index}
-        />
-      ))}
+    <div className="grid grid-cols-5 grid-rows-5 gap-0 h-64 w-64">
+      {Array.from({ length: 5 }, (_, row) =>
+        Array.from({ length: 5 }, (_, col) => (
+          <CellGrid
+            color={cellColor[row][col]}
+            key={`${row}-${col}`}
+            onDrop={handleDrop}
+            row={row}
+            col={col}
+          />
+        ))
+      )}
     </div>
   );
+
 };
+
 
 const DragAndDropComponent = () => {
   const [currentColor, setCurrentColor] = useState([
@@ -95,17 +122,36 @@ const DragAndDropComponent = () => {
     "yellow",
     "green",
   ]);
-  const [cellColor, setCellColor] = useState(Array(25).fill("gray")); // Initialize all cells as gray
+  const [cellColor, setCellColor] = useState(Array.from({ length: 5 }, () => Array(5).fill("gray")));
 
-  const handleDrop = (index, color, id) => {
+  const handleDrop = (row, col, color, id, blockType) => {
     setCellColor((prevColors) => {
       const newColors = [...prevColors];
-      newColors[index] = color;
+      newColors[row][col] = color;
+      const element = BlockTypes[blockType];
+      if (element.left > 0) {
+        for (let i = 0; i < element.left; i++) {
+          newColors[row][col - i - 1] = color;
+        }
+      }
+      if (element.right > 0) {
+        for (let i = 0; i < element.right; i++) {
+          newColors[row][col + i + 1] = color;
+        }
+      }
+      if (element.top > 0) {
+        for (let i = 0; i < element.top; i++) {
+          newColors[row - i - 1][col] = color;
+        }
+      }
+      if (element.bottom > 0) {
+        for (let i = 0; i < element.bottom; i++) {
+          newColors[row + i + 1][col] = color;
+        }
+      }
       return newColors;
     });
-
-    console.log(color, id);
-
+    console.log(blockType)
     setCurrentColor((prevColor) => {
       const colorCycle = {
         blue: "red",
@@ -120,18 +166,21 @@ const DragAndDropComponent = () => {
   };
 
   return (
-    <div className="flex justify-around p-20">
+    <div className="flex flex-wrap justify-around p-20">
       <div>
-        <DraggableItem id="0" color={currentColor[0]} />
-        <DraggableItem id="1" color={currentColor[1]} />
-        <DraggableItem id="2" color={currentColor[2]} />
-        <DraggableItem id="3" color={currentColor[3]} />
+        <DraggableItem id="0" color={currentColor[0]} blockType="Single" />
+        <DraggableItem id="1" color={currentColor[1]} blockType="T" />
+        <DraggableItem id="2" color={currentColor[2]} blockType="I" />
+        <DraggableItem id="3" color={currentColor[3]} blockType="L" />
       </div>
       <DropGrid cellColor={cellColor} handleDrop={handleDrop} />
       <div>
-        Color cycling works, Items are separate Items need to be Tetris shape
-        and affect fields with those shapes. For starters, 1 item is L, another
-        T
+        humble beginings, now i need to:
+        fortify borders of item,
+        display item on left,
+        make sure items wont overlap,
+        make sure items wont escape boundries,
+        items on left start from center ?
       </div>
     </div>
   );
